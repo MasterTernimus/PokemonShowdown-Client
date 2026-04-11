e87234d5e20865f69e9d6cf8d7cf5f0239af127f
Master Ternimus <chinmaya.kallaje7@gmail.com>
2026-04-11 14:08:12 +0530

    Update replay-embed.template.js

--- play.pokemonshowdown.com/js/replay-embed.template.js
+++ play.pokemonshowdown.com/js/replay-embed.template.js
@@ -26,29 +26,28 @@ function requireScript(url) {
 	var scriptEl = document.createElement('script');
 	scriptEl.src = url;
 	document.head.appendChild(scriptEl);
-}
 
 linkStyle('https://play.pokemonshowdown.com/style/font-awesome.css?');
-linkStyle('https://play.pokemonshowdown.com/style/battle.css?a7');
-linkStyle('https://play.pokemonshowdown.com/style/replay.css?a7');
-linkStyle('https://play.pokemonshowdown.com/style/utilichart.css?a7');
+linkStyle('https://play.pokemonreborn-showdown.xyz/style/battle.css?a7');
+linkStyle('https://play.pokemonreborn-showdown.xyz/style/replay.css?a7');
+linkStyle('https://play.pokemonreborn-showdown.xyz/style/utilichart.css?a7');
 
-requireScript('https://play.pokemonshowdown.com/js/lib/ps-polyfill.js');
-requireScript('https://play.pokemonshowdown.com/config/config.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/lib/jquery-1.11.0.min.js');
-requireScript('https://play.pokemonshowdown.com/js/lib/html-sanitizer-minified.js');
-requireScript('https://play.pokemonshowdown.com/js/battle-sound.js');
-requireScript('https://play.pokemonshowdown.com/js/battledata.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex-mini.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex-mini-bw.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/graphics.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/moves.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/abilities.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/items.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/teambuilder-tables.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/battle-tooltips.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/battle.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/js/lib/ps-polyfill.js');
+requireScript('https://play.pokemonreborn-showdown.xyz/config/config.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/js/lib/jquery-1.11.0.min.js');
+requireScript('https://play.pokemonreborn-showdown.xyz/js/lib/html-sanitizer-minified.js');
+requireScript('https://play.pokemonreborn-showdown.xyz/js/battle-sound.js');
+requireScript('https://play.pokemonreborn-showdown.xyz/js/battledata.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/data/pokedex-mini.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/data/pokedex-mini-bw.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/data/graphics.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/data/pokedex.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/data/moves.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/data/abilities.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/data/items.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/data/teambuilder-tables.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/js/battle-tooltips.js?a7');
+requireScript('https://play.pokemonreborn-showdown.xyz/js/battle.js?a7');
 
 var Replays = {
 	battle: null,
